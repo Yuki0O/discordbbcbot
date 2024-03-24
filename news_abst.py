d01d7681ad9fc854daec2afff72d3f4384916019
DESKTOP-HDEDGBG\YUKI <snowpione@gmail.com>
2024-03-24 23:47:06 +0900

    いちじきゅうせん

--- news_abst.py
+++ news_abst.py
@@ -1,15 +1,22 @@
 import aiohttp
 from bs4 import BeautifulSoup
-import random
-import discord
+import asyncio
 import re
-from googletrans import Translator
 
-async def news_abst():
-    url = "https://www.bbc.com/news/science-environment-68110310"
+async def news_data(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as res:
-            article_soup = BeautifulSoup(res.text(), 'html.parser')
-    return article_soup.text
+            article_html = await res.text()
+            soup = BeautifulSoup(article_html, 'html.parser')
+            body_element = soup.find_all("b")
+            if body_element:   
+                return str(body_element)
+            else:
+                return "No body"
+            
+async def main():
+    article_url = "https://www.bbc.com/news/science-environment-68110310"
+    body_search = await news_data(article_url)
+    print (body_search)
 
-loop = news_abst()
+asyncio.run(main())
