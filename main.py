c4bd5d90c1f3bf9ea35b284952df5db2ce8d3834
Yuki0O <snowpione@gmail.com>
2024-03-25 17:59:46 +0900

    clean:行や不要コメントの整理

--- main.py
+++ main.py
@@ -51,7 +51,10 @@ async def news_scrap_NGG() -> list[str]:
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as res:
             soup = BeautifulSoup(await res.text(), 'html.parser')
-    article_links = soup.find_all(class_=["firstPageFirstItem FREE","firstPageBorderItem ","FREE"], limit=11)
+    article_links = soup.find_all(
+        class_=["firstPageFirstItem FREE",
+                "firstPageBorderItem ",
+                "FREE"], limit=11)
     tops = "https://natgeo.nikkeibp.co.jp/"
     titles_urls = []
     random_num = random.randint(0, 9)
@@ -60,7 +63,6 @@ async def news_scrap_NGG() -> list[str]:
     article_title = article_soup.find('h3').get_text()
     article_absts = article_soup.find('p').get_text()
     article_url = tops + article_soup.find('a')['href']
-    #article_url = tops + article_soup.find(href=re.compile("atcl")).get_text()
     titles_urls.append(f'『{article_title}』\n{article_absts}\n{article_url}')
     return titles_urls
 
